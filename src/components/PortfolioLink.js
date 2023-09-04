9d5e61b72d1e68aab8d8c2ecd9b216d97e94670b
TristanPagden <tristanpagden@gmail.com>
2023-09-04 19:38:27 +0100

    commit 16

--- src/components/PortfolioLink.js
+++ src/components/PortfolioLink.js
@@ -10,6 +10,7 @@ const Portfolio = () => {
         <h1 className={styles.title}>Portfolio</h1>
         <Project props={{text: 'This is my personal website, which you are currently on. This displays my projects and tells you about me!', title: 'My Personal Website', languages: 'Next.js VanillaCSS', link: '/', code: 'https://github.com/TristanPagden/personalwebsite', image: '/projects/project1.png', slide: '100vw'}}/>
         <Project props={{text: 'This is a fullstack webapp that I created. It uses AI in order to make predictions about stock prices.', title: 'Stock Price Predictor', languages: 'Next.js Tailwind Express Tensorflow', link: 'https://stockpricepredictor.vercel.app', code: 'https://github.com/TristanPagden/stockpricepredictor', image: '/projects/project2.png', slide: '-100vw'}}/>
+        <Project props={{text: 'This is a website for one of my clients a building company to display what their company offers to clients. ', title: 'Mote Building and Construction', languages: 'Next.js Tailwind', link: 'https://motebuilding.vercel.app', code: 'https://github.com/TristanPagden/moteconstruction', image: '/projects/project3.png', slide: '100vw'}}/>
       </div>
     </div>
   )
