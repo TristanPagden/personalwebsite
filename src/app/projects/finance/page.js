5e0340b52ce41e1dc2abf8dd57d63e76f7bb8bed
TristanPagden <tristanpagden@gmail.com>
2024-04-12 20:20:30 +0100

    commit 4

--- src/app/projects/finance/page.js
+++ src/app/projects/finance/page.js
@@ -7,6 +7,7 @@ const finance = () => {
       <div className='w-full'>
         <Project props={{title: 'Pivot point trading strategy', codeLink: 'https://github.com/TristanPagden/alpaca-pivot-points', demoLink:'https://replit.com/@tristanpagden/alpaca-pivot-points', image: '/projects/finance/pivot_points.jpeg'}}></Project>
         <Project props={{title: 'AI trading strategies with Pytorch', codeLink: 'https://github.com/TristanPagden/aplaca-ai-strategies', demoLink:'https://replit.com/@tristanpagden/aplaca-ai-strategies', image: '/projects/finance/ai_alpaca.jpg'}}></Project>
+        <Project props={{title: 'Time series stock prediction', codeLink: 'https://github.com/TristanPagden/time-series-stock-prediction', demoLink:'https://replit.com/@tristanpagden/time-series-stock-prediction', image: '/projects/finance/time_series.jpg'}}></Project>
       </div>
     </div>
   )
