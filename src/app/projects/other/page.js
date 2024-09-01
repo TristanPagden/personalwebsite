ea5f0f6f47e40a7bddcec983f486049dde15a54c
Tristan Pagden <tristanpagden@gmail.com>
2024-09-01 18:36:02 +0100

    Update page.js

--- src/app/projects/other/page.js
+++ src/app/projects/other/page.js
@@ -13,6 +13,7 @@ const other = () => {
       </div>
       <br />
       <div className='w-full'>
+        <Project props={{title: 'Chemical engineering simulation', codeLink: 'https://github.com/TristanPagden/ChemicalEngSim', demoLink:''}}></Project>
         <Project props={{title: 'Page rank algorithm', codeLink: 'https://replit.com/@tristanpagden/Page-rank-algorithm', demoLink:'https://replit.com/@tristanpagden/Page-rank-algorithm', image: '/projects/other/page_rank.jpg'}}></Project>
         <Project props={{title: '7 degrees of Kevin Bacon', codeLink: 'https://replit.com/@tristanpagden/7-degrees-of-Kevin-Bacon', demoLink:'https://replit.com/@tristanpagden/7-degrees-of-Kevin-Bacon', image: '/projects/other/degrees.jpeg'}}></Project>
         <Project props={{title: 'Knights and Knaves logic puzzle', codeLink: 'https://replit.com/@tristanpagden/Knights-and-Knaves-logic-puzzle', demoLink:'https://replit.com/@tristanpagden/Knights-and-Knaves-logic-puzzle', image: '/projects/other/knights.jpeg'}}></Project>
